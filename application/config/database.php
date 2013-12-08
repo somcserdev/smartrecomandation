95b6c755eadafd7eabc1f65320f6ed929657fab4
Chris Cheng <chrischeng03@qq.com>
2013-12-08 08:35:55 +0800

    update to new domain to access the net

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'zhu88jie';
+$db['default']['password'] = '';
 $db['default']['database'] = 'user_preference';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
