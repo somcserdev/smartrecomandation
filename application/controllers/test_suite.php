6ea9203dbb6bbf07a9f986703d7786c7c21ce341
Jiao, Yan <jiaoyan1983123@hotmail.com>
2013-12-12 23:49:43 +0800

    http request formation

--- application/controllers/test_suite.php
+++ application/controllers/test_suite.php
@@ -1,5 +1,7 @@
 <?php
+
 require_once 'comm/html.php';
+
 class Test_suite extends Html_Controller {
 
     protected function _initialize() {
@@ -14,11 +16,28 @@ class Test_suite extends Html_Controller {
     }
 
     private function test_signature() {
-        $map = array('name' => 'jiaoyan', 'age' => '32');
+        $map = array('name' => 'sasa', 'age' => '32');
         $result = signature($map);
 
-        $expected_result = sha1('namejiaoyanage32security_key705582a3db9bd2ee');
+        $expected_result = sha1('namesasaage32security_key705582a3db9bd2ee');
         $this->unit->run($result, $expected_result, 'signature');
+    }
+
+    public function test_get_req_url() {
+        $server_url = '';
+        $query_map = array('name' => 'sasa', 'age' => '32', 'comment' => '你好');
+
+        $this->load->library('http');
+        $query_str = '';
+        foreach ($query_map as $key => $value) {
+            if ($key == "comment" || $key == "keyword" || $key == "Message") {
+                $value = urlencode($value);
+            }
+            $query_str .= $key . '=' . $value . '&';
+        }
+        $query_str .= 'sig=' . signature($query_map);
+        $response = $this->http->get($server_url . '?' . $query_str);
+        $this->output->set_output($response);
     }
 
 }
